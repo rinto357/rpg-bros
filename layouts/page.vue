a5112063b08e010bcc79b2ea084e230a453c1fc1
rinto357 <5124172+rinto357@users.noreply.github.com>
2019-10-19 02:42:28 -0700

    Update page.vue

--- layouts/page.vue
+++ layouts/page.vue
@@ -1,5 +1,10 @@
 <template>
-  <article class="article content">
-    <slot name="default"/>
-  </article>
+  <header></header>
+  <main class="page">
+    <article class="main-content">
+      <header class="article-title">{{page.frontmatter.title}}</header>
+      <slot name="default"/>
+    </article>
+  </main>
+  <footer></footer>
 </template>

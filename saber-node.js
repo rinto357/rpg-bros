fdc42f46eeb9906c87d798cb171da9ed41fd35c9
rinto357 <5124172+rinto357@users.noreply.github.com>
2019-11-09 00:00:11 -0800

    testing netlify cms

--- saber-node.js
+++ saber-node.js
@@ -3,5 +3,6 @@ const path = require('path');
 exports.chainWebpack = function(chain) {
   // add an alias
   chain.resolve.alias.set('@comp',path.resolve(__dirname,'./components/'))
+  chain.resolve.alias.set('@lay',path.resolve(__dirname,'./layouts/'))
 }
 // alias does not work for components or layouts in theme folder, as relative paths are sufficient
---
+++ static/admin/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="admin panel">
+  </div>
+</template>
+<script>
+import cms from 'netlify-cms'
+import Page from '@lay/page.vue'
+export default {
+  extends: Page
+}
+</script>
